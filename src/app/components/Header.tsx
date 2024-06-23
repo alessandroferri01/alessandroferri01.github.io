cd7fca1810c29a1aebfcd5db60df51c72698707a
AleFerri01pd <alessandro.ferri@business-changers.it>
2024-06-24 00:15:35 +0200

    modify header button in mobile

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,10 +1,17 @@
+'use client'
+
 import React from "react";
 import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button } from "@nextui-org/react";
 import Logo from "@/icons/Logo";
 import ThemeSwitch from "./ThemeSwitch";
 
+//Custom Hooks
+import { useIsMobile } from "@/hooks/useIsMobile";
+
 
 export default function Header() {
+  const isMobile = useIsMobile()
+
   return (
     <Navbar 
       style={{ height: "5.5rem", position: "sticky" }} 
@@ -15,12 +22,12 @@ export default function Header() {
         </Link>
       </NavbarBrand>
       <NavbarContent justify="end">
-        <NavbarItem>
+        <NavbarItem style={{display: !isMobile ? 'block' : 'none'}}>
           <Button as={Link} color="primary" href="#" variant="flat">
             Say Hello 😎
           </Button>
         </NavbarItem>
-        <NavbarItem>
+        <NavbarItem style={{display: !isMobile ? 'block' : 'none'}}>
           <Button as={Link} color="secondary" href="/CV_Alessandro_Ferri.pdf" target="blank" variant="flat">
             Download CV 📁
           </Button>
